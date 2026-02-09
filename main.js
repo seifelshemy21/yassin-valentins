a34f06b6b0984bea2ea82bd60e9fb5a34cacb15c
seifelshemy21 <seifelshemy1@gmail.com>
2026-02-09 20:16:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault(); // يمنع الـ refresh
+
+  let password = document.getElementById("pass").value.trim();
+
+  if (password === "18102024") {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("content").classList.remove("hidden");
+    typeText();
+    typeTextSip();
+  } else {
+    document.getElementById("error").innerText = "Wrong password 😢";
+  }
+});
+
+let text = "You are the most beautiful thing in my life ❤️";
+let i = 0;
+
+function typeText() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeText, 50);
+  }
+}
+
+let text_sip = "Happy Valentine’s Day my love Every day with you feels special but today is just another reminder of how lucky I am to have you in my life You’re not just my girlfriend you’re my comfort my happiness, and the reason I smile without even noticing Thank you for being there for me, for understanding me and for loving me the way you do I hope we stay together keep growing together and keep making beautiful memories side by side I love you more than words can explain, and I’m grateful that I get to call you mine❤️🫂";
+let x = 0;
+
+function typeTextSip() {
+  if (x < text_sip.length) {
+    document.getElementById("typing_sip").innerHTML += text_sip.charAt(x);
+    x++;
+    setTimeout(typeTextSip, 50);
+  }
+}
+
+function showSurprise() {
+  document.getElementById("surprise").classList.remove("hidden");
+}
